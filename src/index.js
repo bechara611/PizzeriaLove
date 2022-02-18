ddefcbc78b6529d6e682df7d7ac8c8507f2dc936
Dany Bechara <dbechara@MCLCRXVFL>
2022-02-18 15:19:38 -0400

    primero

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const Index = () => {
   return ( <div>Dany</div> );
 }
  
-export default Index;
+
 ReactDOM.render(<Index />,document.getElementById('root')
 );
 
